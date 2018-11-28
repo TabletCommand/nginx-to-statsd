0792e315b65034f41f23abe27d556cbeb8a4337f
Marius Ursache <marius@marius.me.uk>
2018-11-28 18:56:43 +1100

    Normalize path (remove uuid, mongoid, query)

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"os"
 	"regexp"
+	"strings"
 
 	"github.com/hpcloud/tail"
 	"gopkg.in/alexcesaro/statsd.v2"
@@ -30,7 +32,122 @@ func ParseNginxLog(line string) (map[string]string, error) {
 		}
 	}
 
+	data["path_normalized"] = NormalizePath(data["path"])
+
 	return data, nil
+}
+
+type c struct {
+	regex   string
+	replace string
+}
+
+func NormalizePath(path string) string {
+	cleanUp := []c{
+		c{
+			regex:   "^/js/.*",
+			replace: "js",
+		},
+		c{
+			regex:   "^/css/.*",
+			replace: "css",
+		},
+		c{
+			regex:   "^/img/.*",
+			replace: "img",
+		},
+		c{
+			regex:   "^/.*\\.php",
+			replace: "php",
+		},
+
+		c{
+			regex:   "[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}",
+			replace: "uuid",
+		},
+		c{
+			regex:   "[0-9a-fA-F]{24}",
+			replace: "mongoid",
+		},
+		c{
+			regex:   "\\?.*$",
+			replace: "/query",
+		},
+		c{
+			regex:   "/+",
+			replace: "/",
+		},
+		c{
+			regex:   "^/",
+			replace: "",
+		},
+		c{
+			regex:   "/$",
+			replace: "",
+		},
+		c{
+			regex:   "/",
+			replace: ".",
+		},
+	}
+
+	var result = strings.ToLower(path)
+
+	for _, item := range cleanUp {
+		var r = regexp.MustCompile(item.regex)
+		result = r.ReplaceAllString(result, item.replace)
+	}
+
+	return result
+}
+
+func verifyFile(fn string) {
+	stopAt := 300
+	normalizedData := make(map[string]int)
+
+	file, err := os.Open(fn)
+	defer file.Close()
+
+	if err != nil {
+		fmt.Printf("Failed opening file: %v\n", err)
+		return
+	}
+
+	// Start reading from the file with a reader.
+	reader := bufio.NewReader(file)
+
+	var line string
+	for {
+		line, err = reader.ReadString('\n')
+		if err != nil {
+			fmt.Printf("Failed reading line: %v\n", err)
+			break
+		}
+
+		if strings.TrimSpace(line) == "" {
+			break
+		}
+
+		stats, err := ParseNginxLog(line)
+		if err != nil {
+			fmt.Printf("Failed to parse line: %v\n", err)
+			break
+		}
+
+		normalized := stats["path_normalized"]
+		normalizedData[normalized]++
+
+		if len(normalizedData) >= stopAt {
+			fmt.Printf("Size > %v\n", stopAt)
+			break
+		}
+	}
+
+	fmt.Printf("normalizedData:\n")
+	for k, v := range normalizedData {
+		fmt.Printf("%v: %v\n", k, v)
+	}
+	fmt.Printf("%v of %v \n", len(normalizedData), stopAt)
 }
 
 func main() {
@@ -48,9 +165,9 @@ func main() {
 	}
 	defer stats.Close()
 
-	seek_info := tail.SeekInfo{Offset: 0, Whence: os.SEEK_END}
+	info := tail.SeekInfo{Offset: 0, Whence: os.SEEK_END}
 	config := tail.Config{
-		Location: &seek_info,
+		Location: &info,
 		Follow:   true,
 		ReOpen:   true,
 	}
--- main_test.go
+++ main_test.go
@@ -17,3 +17,22 @@ func TestParseNginxLog(t *testing.T) {
 		t.Error("Expected", "200", "got", data["code"])
 	}
 }
+
+func TestNormalizePath(t *testing.T) {
+	data := map[string]string{
+		"/api/sync/items/A54E6F3D-2850-434C-AFDA-D138627A89AE": "api.sync.items.uuid",
+		"/api/sync/items/?modified_unix_date=15":               "api.sync.items.query",
+		"/api/sync/items/592ba375e47dac145f680bab":             "api.sync.items.mongoid",
+		"/js/external/ie10-viewport-bug-workaround.js":         "js",
+		"/css/file.min.css":                                    "css",
+		"/img/image.png":                                       "img",
+		"/system.php":                                          "php",
+	}
+
+	for input, output := range data {
+		result := NormalizePath(input)
+		if result != output {
+			t.Error("Expected", output, "got", result, "for", input)
+		}
+	}
+}
